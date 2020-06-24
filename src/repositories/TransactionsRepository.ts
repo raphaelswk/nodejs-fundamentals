6a43189e9c100eb20209109d475bed0298ee01f8
Raphael Socolowski <raphael.swk@gmail.com>
2020-06-24 19:54:22 +0100

    Add transactions, get transactions and balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeBalance = this.transactions.reduce((prev, cur) => {
+      return cur.type === 'income' ? prev + cur.value : prev;
+    }, 0);
+    const outcomeBalance = this.transactions.reduce((prev, cur) => {
+      return cur.type === 'outcome' ? prev + cur.value : prev;
+    }, 0);
+    const totalBalance = incomeBalance - outcomeBalance;
+
+    const balance = {
+      income: incomeBalance,
+      outcome: outcomeBalance,
+      total: totalBalance,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'outcome') {
+      const balance = this.getBalance();
+      if (balance.total < value) {
+        throw Error('The balance is not enough for the outcome');
+      }
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
